929b6b8bb8194a1cdc29de13815e7e683a37e800
neeraj2117 <royal.nanu21@gmail.com>
2025-09-29 12:53:18 +0530

    Add config inside client

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -5,8 +5,8 @@ function AuthLayout() {
         <div className="flex font-[Rajdhani] min-h-screen w-full">
             <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
                 <div className="max-w-md space-y-6 text-center text-primary-foreground">
-                    <h1 className="text-4xl font-extrabold tracking-tight">
-                        Welcome to Eleganza Shopping!!
+                    <h1 className="text-[35px] font-extrabold tracking-tight">
+                        Welcome to Eleganza!!
                     </h1>
                 </div>
             </div>
